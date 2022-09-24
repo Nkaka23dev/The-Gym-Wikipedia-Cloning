5f02286b105cf07256255ba2b59dfe01b515dcf5
Nkaka23dev <nkakaeric96@gmail.com>
2022-09-24 23:40:29 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bgBtn: "#d40356",
+        modYell: "#fffd33",
+        khaki: "#E3DFD8"
+      }
+    },
+  },
+  plugins: [],
+}
